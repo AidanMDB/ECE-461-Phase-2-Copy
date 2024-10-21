2b3b76163c8a8f272b74e1813a7e998b0cfe4c73
Margaret Solinsky <msolinsk@purdue.edu>
2024-10-21 08:55:23 -0400

    added inputs for login/signup

--- my-amplify-typescript-app/src/components/Login.tsx
+++ my-amplify-typescript-app/src/components/Login.tsx
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Login.css';
 
 function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <div className='login'>
       <header>
@@ -9,7 +12,22 @@ function Login() {
       </header>
       <body>
         <p>Enter Username:</p>
+        <input
+          type="text" 
+          value={username} 
+          onChange={e => setUsername(e.target.value)}
+          className="input"
+          placeholder=""
+        ></input>
         <p>Enter Password:</p>
+        <input
+          type="text" 
+          value={password} 
+          onChange={e => setPassword(e.target.value)}
+          className="input"
+          placeholder=""
+        ></input>
+        <br />
         <button className="search-button">Login</button>
         <p>Not already a member?</p>
         <a href="/SignUp"><button className="search-button">Sign Up</button></a>
--- my-amplify-typescript-app/src/components/SignUp.tsx
+++ my-amplify-typescript-app/src/components/SignUp.tsx
@@ -1,18 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './SignUp.css'
 
 function SignUp() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [rePassword, setRePassword] = useState("");
+  const [email, setEmail] = useState("");
+
+  async function handleSubmit() {
+    console.log("user sign up!")
+    console.log(username)
+    console.log(password)
+    console.log(rePassword)
+    console.log(email)
+  }
+
   return (
     <div className="signUp">
       <header>
         <h1>Sign Up Page</h1>
       </header>
       <body>
+        <form className='signUp-form' onSubmit={handleSubmit}>
         <p>Enter Username:</p>
+        <input
+          type="text" 
+          value={username} 
+          onChange={e => setUsername(e.target.value)}
+          className="input"
+          placeholder=""
+        ></input>
         <p>Enter Password:</p>
+        <input
+          type="text" 
+          value={password} 
+          onChange={e => setPassword(e.target.value)}
+          className="input"
+          placeholder=""
+        ></input>
         <p>Re-enter Password:</p>
+        <input
+          type="text" 
+          value={rePassword} 
+          onChange={e => setRePassword(e.target.value)}
+          className="input"
+          placeholder=""
+        ></input>
         <p>Enter Email:</p>
-        <a href="/"><button className="search-button">Sign Up</button></a>
+        <input
+          type="text" 
+          value={email} 
+          onChange={e => setEmail(e.target.value)}
+          className="input"
+          placeholder=""
+        ></input>
+        <br />
+        <button type="submit" className="search-button" >Sign Up</button>
+        {/* <a href="/"><button className="search-button">Sign Up</button></a> */}
+        </form>
       </body>
     </div>
   );
