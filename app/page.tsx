68f4f8cadea5963f3c384700a29cc26766df635c
Saniya Garg <111753836+gsaniya@users.noreply.github.com>
2024-12-06 13:34:20 -0500

    Front-end finished and fully tested (#31)
    * added front-end files
    
    * removed purple
    
    * homepage updates
    
    * signup page fixed
    
    * final css & back button
    
    * added upload link and toggle
    
    * slider overlapping text issue fixed.
    
    * upload package UI done
    
    * update package functionality for front-end finished.
    
    * adding titles for ADA compliance
    
    * testing with selenium started; need to finish error handling and rest of test cases.
    
    * testing upload page
    
    * corrected remaining upload tests
    
    * selenium testing almost fully finished; need to fix some update package issues.
    
    * fixed small test issues
    
    * fixed frontend errors & upload page toggle
    
    * fixed frontend debloat test
    
    ---------
    
    Co-authored-by: Margaret Solinsky <msolinsk@purdue.edu>
    Co-authored-by: Margaret Solinsky <111827563+msolinsky@users.noreply.github.com>

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,8 @@ import type { Schema } from "@/amplify/data/resource";
 import "./../app/app.css";
 import { Amplify } from "aws-amplify";
 import outputs from "@/amplify_outputs.json";
-import "@aws-amplify/ui-react/styles.css";
+// import "@aws-amplify/ui-react/styles.css";
+import "./globals.css";
 //import { useAuthenticator } from "@aws-amplify/ui-react";
 import Link from "next/link";
 
@@ -34,17 +35,29 @@ export default function App() {
   }, []);
 
   return (
-    <main>
-      <h1>Packages</h1>
-      <ul>
-        {packages.map((pkg) => (
-          <li key={pkg.ID}>{pkg.Name} : {pkg.Version}</li>
-        ))}
-      </ul>
-      <div>
-      </div>
-      <Link href="/upload"><button>Go to Upload File Page</button></Link>
+    <main className="App">
+      <title>Package Manager</title>
+      <header className="App-header">
+        <div className="login-container">
+          <Link className="sign-up" href="/login"><button className="login-button">Login</button></Link>
+        </div>
+        <h1 className="title">Package Manager</h1>
+      </header>
     </main>
+    // <main>
+    //   <div className="login-container">
+    //     <Link className="sign-up" href="/login"><button className="login-button">Login</button></Link>
+    //   </div>
+    //   <h1>Packages</h1>
+    //   <ul>
+    //     {packages.map((pkg) => (
+    //       <li key={pkg.ID}>{pkg.Name} : {pkg.Version}</li>
+    //     ))}
+    //   </ul>
+    //   {/* <div>
+    //     <Link href="/upload"><button>Go to Upload File Page</button></Link>
+    //   </div> */}
+    // </main>
   );
 }
 
