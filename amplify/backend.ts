58dd477457a3692b43da9aa6592d68cc63c2e256
AidanMDB <121575554+AidanMDB@users.noreply.github.com>
2024-12-08 00:57:57 -0500

    added /packages api back in

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -97,6 +97,20 @@ const apiPackages = new LambdaIntegration(
   backend.myApiPackages.resources.lambda
 )
 
+// add lambda integration for regex search api
+const lambdaIntegrationRegex = new LambdaIntegration(
+  backend.myApiFunctionRegex.resources.lambda
+);
+
+
+// create new API path for /packages
+const packagesPath = myRestApi.root.addResource('packages');
+
+packagesPath.addMethod('POST', apiPackages, {
+
+});
+
+
 // create new API path
 const packagePath = myRestApi.root.addResource('package');
 
@@ -128,15 +142,11 @@ registerPath.addMethod('POST', lambdaIntegrationRegister, {
 // create new API path for user authenticate
 const authenticatePath = myRestApi.root.addResource('authenticate');
 
-authenticatePath.addMethod('POST', lambdaIntegrationAuthenticate, {
+authenticatePath.addMethod('PUT', lambdaIntegrationAuthenticate, {
 
 });
 
 
-// add lambda integration for regex search api
-const lambdaIntegrationRegex = new LambdaIntegration(
-  backend.myApiFunctionRegex.resources.lambda
-);
 
 // create new API path for regex search
 const regexPath = myRestApi.root.addResource('package').addResource('byRegEx');
